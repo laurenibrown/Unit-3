534464714a21bc07fbfdb8cdf4321324845035d3
Lauren Brown <laurenbrown1886@gmail.com>
2023-05-06 12:44:48 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/cosc338unit2project/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cosc338unit2project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
